73e0355ffdd9cc60b87e49c696e7ade4502d9e1b
Sarah Meyer <sarah.meyer@formidable.com>
2023-07-26 12:31:28 -0600

    initial site set up

--- app/page.js
+++ app/page.js
@@ -1,94 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css"
+import starStyles from "./stars.module.css"
+import { Sacramento } from "next/font/google"
+import { Montserrat } from "next/font/google"
+
+const sacramento = Sacramento({
+  weight: "400",
+  subsets: ["latin"],
+})
+const montserrat = Montserrat({
+  weight: "400",
+  subsets: ["latin"],
+})
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main className={starStyles.main}>
+      <div className={starStyles.stars}></div>
+      <div className={starStyles.stars2}></div>
+      <div className={starStyles.stars3}></div>
+      <div className={`${montserrat.className} ${styles.main}`}>
+        <div className={styles.heading}>Hello World,</div>
+        <h1 className={sacramento.className}>I'm Sarah Egan</h1>
+        <h2 className={styles.subTitle}>
+          Software Engineer & Engineering Manager
+        </h2>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          As a lifelong learner, I am always eager to embrace new challenges and
+          technologies. I believe that the key to successful software
+          engineering lies in staying curious and adaptable, keeping up with the
+          latest trends while appreciating the timeless fundamentals.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
